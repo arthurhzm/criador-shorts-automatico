cc747f2dbfd01e9494007825de0fe42e4262a169
Arthur Marena <arthurzhm2@gmail.com>
2024-12-03 07:15:42 -0300

    FEAT: cortando os momentos no vídeo

--- src/detectar_picos.py
+++ src/detectar_picos.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
+import os
 
 def detectar_picos_audio(caminho_audio, min_duracao=500, limiar_silencio=-30):
     """
@@ -14,6 +15,27 @@ def detectar_picos_audio(caminho_audio, min_duracao=500, limiar_silencio=-30):
     picos = detect_nonsilent(audio, min_silence_len=min_duracao, silence_thresh=limiar_silencio)
     return picos
 
+def cortar_trechos_video(video_path, momentos, output_folder='shorts'):
+    """
+        Corta trechos do vídeo em que há momentos relevantes.
+        - video_path: Caminho do vídeo original.
+        - momentos: Lista de intervalos [(inicio, fim), ...] em milissegundos.
+        - output_folder: Pasta onde os trechos serão salvos.
+    """
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for i, (inicio, fim) in enumerate(momentos):
+        inicio_seg = inicio / 1000
+        fim_seg = fim / 1000
+        output_file = os.path.join(output_folder, f"short_{i + 1}.mp4")
+
+        os.system(f'ffmpeg -i "{video_path}" -ss {inicio_seg} -to {fim_seg} -c copy "{output_file}"')
+        print(f"Trecho {i + 1} salvo em {output_file}")
+
+
 caminho_audio = "audios/E se o YURI ALBERTO fosse para o REAL MADRID？ GANHARIA TUDO？.mp3"
 momentos_relevantes = detectar_picos_audio(caminho_audio)
-print("Momentos relevantes (ms):", momentos_relevantes)
+
+video_path = "videos/E se o YURI ALBERTO fosse para o REAL MADRID？ GANHARIA TUDO？.mp4"
+cortar_trechos_video(video_path, momentos_relevantes)
